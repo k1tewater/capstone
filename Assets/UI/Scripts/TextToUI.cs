d7bb06fccb9b58203265aa33a9a3310802d47d46
namyunT <nyt4564@gmail.com>
2024-09-11 15:47:10 +0900

    TextToUI( 레이아웃 추가)
    1.input 버튼을 누르면
    - (Text to 의 overlay_bottom을 보이게 만듬)
    2.overlay_bottom의 yes no 버튼
    yes버튼(예전 인풋버튼 기능 수행)
    no버튼(오버레이 안보이게 바꿈)

--- Assets/UI/Scripts/TextToUI.cs
+++ Assets/UI/Scripts/TextToUI.cs
@@ -1,21 +1,38 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class TextToUI : UIManager
 {
+    
     protected override void Awake()
     {
-        buttonNames = new string[] { "Input" };
-        clickEvts = new EventCallback<ClickEvent>[] { ClickInput };
+        buttonNames = new string[] { "Input","alarm_yes","alarm_no" };
+        clickEvts = new EventCallback<ClickEvent>[] { ClickInput ,ClickYes,ClickNo};
         base.Awake();
         document.rootVisualElement.style.display = DisplayStyle.None;
+        VisualElement overlay = document.rootVisualElement.Q<VisualElement>("overlay_bottom");
+        overlay.style.display = DisplayStyle.None;
+
     }
 
 
     void ClickInput(ClickEvent evt)
     {
+        VisualElement overlay = document.rootVisualElement.Q<VisualElement>("overlay_bottom");
+        overlay.style.display = DisplayStyle.Flex;
+  
+    }
+    void ClickYes(ClickEvent evt)
+    {
         TextField text = document.rootVisualElement.Q<TextField>("InputTextField");
         Debug.Log(text.value);
         Debug.Log("Input clicked");
+    }
+
+    void ClickNo(ClickEvent evt)
+    {
+        VisualElement overlay = document.rootVisualElement.Q<VisualElement>("overlay_bottom");
+        overlay.style.display = DisplayStyle.None;
     }
 }
